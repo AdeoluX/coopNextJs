bb50603486c4a49e8faf55da2d7b34c8178d4fed
AdeoluX <adeolutayox@gmail.com>
2025-10-15 22:06:41 +0100

    new

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_BASE:
-      process.env.NEXT_PUBLIC_API_BASE || "http://localhost:3004",
+      process.env.NEXT_PUBLIC_API_BASE ||
+      "https://coopsaas.onrender.com" ||
+      "http://localhost:3004",
   },
 };
 
